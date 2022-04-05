dd4200ac80dd70526af2002b4a05bfa0db63d396
mmikriukov <>
2022-04-05 15:46:23 +0500

    init project

---
+++ main/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { TuiRootModule, TuiDialogModule, TuiNotificationsModule } from "@taiga-ui/core";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+      TuiRootModule,
+      BrowserAnimationsModule,
+      TuiDialogModule,
+      TuiNotificationsModule
+],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
